1dd23baf545aa5f871aaec1d5e499599fc135e46
Yuzhong23 <xixixixifatur@gmail.com>
2024-04-25 10:31:00 +0700

    Membuat int nim, string nama dan node pada int addNode

--- 157_singlelinkedlist.cpp
+++ 157_singlelinkedlist.cpp
@@ -10,7 +10,15 @@ struct Node {
 Node* START = NULL;
 
 void addNode() {
-
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
 }
 
 bool searchNode(int nim, Node* current, Node* previous) {
