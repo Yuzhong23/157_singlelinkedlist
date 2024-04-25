0eea04182f1aba8eac251494623465419c5f4a2f
Yuzhong23 <xixixixifatur@gmail.com>
2024-04-25 10:30:09 +0700

    Membuat prosedure void addNode, bool searchNode, bool deleteNod, bool listEmpt, void traverse, void searchData.

--- 157_singlelinkedlist.cpp
+++ 157_singlelinkedlist.cpp
@@ -7,6 +7,33 @@ struct Node {
     Node* next;
 };
 
+Node* START = NULL;
+
+void addNode() {
+
+}
+
+bool searchNode(int nim, Node* current, Node* previous) {
+
+}
+
+bool deleteNode(int nim) {
+
+}
+
+bool listEmpty() {
+
+}
+
+
+void traverse() {
+
+}
+
+void searchData() {
+
+}
+
 int main() {
 	
 }
