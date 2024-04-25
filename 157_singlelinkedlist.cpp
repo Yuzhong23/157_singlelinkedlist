d96b91091c20d041556ac9c38f958b4668bcd157
Yuzhong23 <xixixixifatur@gmail.com>
2024-04-25 10:41:55 +0700

    Membuat case 2 pada int main

--- 157_singlelinkedlist.cpp
+++ 157_singlelinkedlist.cpp
@@ -152,6 +152,26 @@ int main() {
                 system("pause");
                 system("cls");
                 break;
+            case 2:
+                if (listEmpty())
+                {
+                    cout << "List kosong" << endl;
+                    system("pause");
+                    system("cls");
+                    break;
+                }
+
+                int nim;
+                cout << "Masukan NIM: ";
+                cin >> nim;
+                if (deleteNode(nim)) {
+                    cout << "nim: " << nim << " berhasil dihapus" << endl;
+                    system("pause");
+                    system("cls");
+                }
+                else
+                    cout << "Data tidak ditemukan" << endl;
+                break;
         }
     }
 }
